54d151d695f93c67e51a2b692a48de0b9f4bc932
정곰 <wjdgpwl1004@naver.com>
2019-09-30 23:38:39 +0900

    최초 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hello from './Hello';
+import './App.css'
 
+//주석
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ const name = 'react';
+ const style ={
+  backgroundColor:'black',
+  color:'aqua',
+  fontSize:24,
+  padding: '1rem'
+ };
+//주석주석
+ return (
+      <Hello name = "react" />
   );
 }
 
