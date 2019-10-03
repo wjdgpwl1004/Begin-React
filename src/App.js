defaaa4fa66c06a8900ba5489c487daa41bae8f3
정곰 <wjdgpwl1004@naver.com>
2019-10-03 23:17:44 +0900

    조건부렌더링

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import Wrapper from './Wrapper';
 function App() {
 
 //주석주석
+//{}로 감싸주는 이유? - true도 자바스크립트 값이기 때문
+// isSpecial 이라고만 하면, 기본값이 true로 들어간다. isSpecial = {true} 와 같음
  return (
     <Wrapper>
-      <Hello name="react" color="red" />
+      <Hello name="react" color="red" isSpecial /> 
       <Hello color="pink" />
     </Wrapper>
   );
--- src/Hello.js
+++ src/Hello.js
@@ -3,12 +3,21 @@ import React from 'react';
 //컴포넌트는 대문자로 시작한다.
 //jsx를 리턴한다.
 //html처럼 생겼지만 html은 아니다.
-function Hello({ color, name }){
+function Hello({ color, name , isSpecial }){
   // console.log(props);
-  return <div style={{
-    color
-  }}>안녕하세요 {name}</div>;
-}
+  console.log(isSpecial);
+  return (
+    //JSX에서 null, undefined, false를 리턴하면 아무것도 나타나지 않는다.
+    //단, 0은 화면에 출력된다.
+    //단순히 숨기고 보이고는 단순계산법 이용, 삼항연산자는 내용이 바뀌는 경우 주로 이용
+    <div style={{ color }}>
+      {/* { isSpecial ? <b>*</b> : null } */}
+      { isSpecial && <b>*</b> }
+      <b>{isSpecial ? '나는 천재다' : '천재가 아니다.'}</b>
+      안녕하세요 {name}
+    </div>
+  );
+};
 
 Hello.defaultProps = {
   name: '이름없음'
