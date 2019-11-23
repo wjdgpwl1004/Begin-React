ef473d9b96a6906b908a5d827685c471f1bec0fd
정곰 <wjdgpwl1004@naver.com>
2019-11-23 19:32:35 +0900

    34. Sass

---
+++ src/Scss.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './components/Button';
+import './App.scss';
+
+function Scss() {
+    return (
+        <div className="App">
+            <div className="buttons">
+                <Button>Button</Button>
+            </div>
+        </div>
+    );
+}
+
+export default Scss; 
--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import * as serviceWorker from './serviceWorker';
 import ContextSample from './ContextSample';
 import Hello from './Hello';
 import Counter from './Counter';
+import Scss from './Scss';
 
 //id가 root인 것을 찾아 리액트 앱을 넣어준다.
 //ReactApp을 랜더링할때는 root엘리먼트(div)안에 랜더링하는 것이다.
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Scss />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
