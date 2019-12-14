0dc40dbb0d9541f0292dc5f86d3da9bd868f3da8
정곰 <wjdgpwl1004@naver.com>
2019-12-14 21:56:54 +0900

    45. Styled-components Dialog 만들기

--- src/StyledComponents.js
+++ src/StyledComponents.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled, {ThemeProvider} from 'styled-components';
 import NewButton from './components/NewButton';
+import Dialog from './components/Dialog';
 
 const AppBlock = styled.div`
     width:512px;
@@ -23,18 +24,35 @@ const palette = {
 }
 
 function StyledComponents() {
+    const [dialog, setDialog] = useState(false);
+    const onClick = () => {
+        setDialog(true);
+    }
+    const onConfirm = () => {
+        setDialog(false);
+    }
+
+    const onCancel = () => {
+        setDialog(false);
+    }
+
     return (
         <ThemeProvider theme={{
             palette
         }}>
+            <>
             <AppBlock>
             <ButtonGroup>
                 <NewButton size="large">Button</NewButton>
                 <NewButton color="gray">Button</NewButton>
                 <NewButton size="small" color="pink" outline>Button</NewButton>
-                <NewButton size="small" color="pink" fullWidth>Button</NewButton>
+                <NewButton size="small" color="pink" fullWidth onClick={onClick}>삭제</NewButton>
             </ButtonGroup>
             </AppBlock>
+            <Dialog title="정말로 삭제하시겠습니까?" confirmText="삭제" cancelText="취소" onConfirm={onConfirm} onCancel={onCancel}  visible ={dialog}>
+                데이터를 정말로 삭제하시겠습니까?
+            </Dialog>
+            </>
         </ThemeProvider>
     );
 }
