7200a6381d3aca7731040d09b97d1cfa7c8eeb0b
정곰 <wjdgpwl1004@naver.com>
2019-12-26 22:56:07 +0900

    47. TodoList - 프로젝트 생성 UI 준비 - 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import ContextSample from './ContextSample';
-import Hello from './Hello';
-import Counter from './Counter';
 import Scss from './Scss';
-import StyledComponents from './StyledComponents';
+import App from './TodoList/App';
 
 //id가 root인 것을 찾아 리액트 앱을 넣어준다.
 //ReactApp을 랜더링할때는 root엘리먼트(div)안에 랜더링하는 것이다.
-ReactDOM.render(<StyledComponents />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
