29da4c229399b04a402c01b7958a60dfecc4a6ec
정곰 <wjdgpwl1004@naver.com>
2020-02-13 00:20:33 +0900

    55. Context 에서 비동기작업 상태 관리하기

--- src/api_starter/User.js
+++ src/api_starter/User.js
@@ -1,27 +1,36 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+// import axios from 'axios';
 import {useAsync} from 'react-async';
+import { useUsersState, useUsersDispatch, getUser } from './UsersContext';
 
-async function getUser({ id }) {
-    const response = await axios.get(`http://jsonplaceholder.typicode.com/users/${id}`);
-    return response.data;
-}
+// async function getUser({ id }) {
+//     const response = await axios.get(`http://jsonplaceholder.typicode.com/users/${id}`);
+//     return response.data;
+// }
 
 function User({ id }) {
-    const {
-        data: user,
-        error,
-        isLoading
-    } = useAsync({
-        //만약 skip true 와 같은 기능을 하고싶다면 run을 불러온다.
-        //defferFn으로 getUser를 넣어준다.
-        promiseFn: getUser,
-        id,
-        watch: id,
-        // reload - refetch와 동일한 기능을 하는 함수
-    });
+    // const {
+    //     data: user,
+    //     error,
+    //     isLoading
+    // } = useAsync({
+    //     //만약 skip true 와 같은 기능을 하고싶다면 run을 불러온다.
+    //     //defferFn으로 getUser를 넣어준다.
+    //     promiseFn: getUser,
+    //     id,
+    //     watch: id,
+    //     // reload - refetch와 동일한 기능을 하는 함수
+    // });
 
-    if(isLoading) return <div>로딩중..</div>
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
+
+    useEffect(() => {
+        getUser(dispatch, id);
+    }, [dispatch, id]);
+    const { loading, data: user, error} = state.user;
+
+    if(loading) return <div>로딩중..</div>
     if(error) return <div>에러 발생</div>
     if(!user) return null;
     return (
--- src/api_starter/Users.js
+++ src/api_starter/Users.js
@@ -1,20 +1,30 @@
 import React, {useState} from 'react';
-import axios from 'axios';
-import useAsync from './useAsync';
+// import axios from 'axios';
+// import useAsync from './useAsync';
 import User from './User';
+import { useUsersState, useUsersDispatch, getUsers } from './UsersContext';
 
-async function getUsers() {
-    const response = await axios.get('http://jsonplaceholder.typicode.com/users/');
-    return response.data;
-}
+
+// async function getUsers() {
+//     const response = await axios.get('http://jsonplaceholder.typicode.com/users/');
+//     return response.data;
+// }
 
 function Users() {
-    const [state, refetch] = useAsync(getUsers, [], true);
+    // const [state, refetch] = useAsync(getUsers, [], true);
     const [userId, setUserId] = useState(null);
-    const {loading, data: users, error} = state;
+    // const {loading, data: users, error} = state;
+    const state = useUsersState();
+    const dispatch = useUsersDispatch();
+    const { loading, data: users, error } = state.users;
+
+    const fetchData = () => {
+        getUsers(dispatch);
+    }
+
     if(loading) return <div>로딩중...</div>
     if(error) return <div>에러발생</div>
-    if(!users) return <button onClick={refetch}>불러오기</button>;
+    if(!users) return <button onClick={fetchData}>불러오기</button>;
 
     return (
         <>
@@ -25,7 +35,7 @@ function Users() {
                     </li>
                 ))}
             </ul>
-            <button onClick={refetch}>다시 불러오기</button>
+            <button onClick={fetchData}>다시 불러오기</button>
             {userId && <User id={userId}/>}
         </>
     );
