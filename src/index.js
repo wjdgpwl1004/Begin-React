8003d45f6f46fc2487865f4b6eedaec367b3927a
정곰 <wjdgpwl1004@naver.com>
2020-02-19 21:10:28 +0900

    57. Router 프로젝트 준비 및 기본적인 사용법

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Scss from './Scss';
-import App from './api_starter/App';
+import App from './react_router/App';
+import { BrowserRouter } from 'react-router-dom';
 
 //id가 root인 것을 찾아 리액트 앱을 넣어준다.
 //ReactApp을 랜더링할때는 root엘리먼트(div)안에 랜더링하는 것이다.
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>, 
+document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
