8f5c5cdb1b51486e38d4de8d0b68e4ead3c6c2f9
정곰 <wjdgpwl1004@naver.com>
2020-02-20 19:54:14 +0900

    58. 파라미터와 쿼리

--- src/react_router/About.js
+++ src/react_router/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import qs from 'qs';
 
-function About() {
+function About({ location }) {
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true
+    })
+    const detail = query.detail === 'true';
+
     return (
         <div>
             <h1>소개</h1>
             <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트 입니다.</p>
+            {detail && <p>detail 값이 true다.</p>}
         </div>
     );
 }
