bb631cd7aa31ee8515b286b37c6d82211048f940
정곰 <wjdgpwl1004@naver.com>
2020-02-24 10:17:22 +0900

    61. 라우터 부가기능 - Switch, NavLink, 기타

--- src/react_router/App.js
+++ src/react_router/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 function App() {
     return (
@@ -17,11 +18,21 @@ function App() {
                 <li>
                     <Link to="/profiles">프로필목록</Link>
                 </li>
+                <li>
+                    <Link to="/history">예제</Link>
+                </li>
             </ul>
             <hr/>
-            <Route path="/" component={Home} exact />
-            <Route path="/about" component={About} />
-            <Route path="/profiles" component={Profiles} />
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/about" component={About} />
+                <Route path="/profiles" component={Profiles} />
+                <Route path="/history" component={HistorySample} />
+                <Route render={({ location }) => <div>
+                    <h2>이 페이지는 존재하지 않습니다.</h2>
+                    <p>{location.pathname}</p>
+                </div>} />
+            </Switch>
         </div>
     );
 }
--- src/react_router/Profiles.js
+++ src/react_router/Profiles.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Profile from './Profile';
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
+import WithRouterSample from './WithRouterSample';
 
 function Profiles() {
     return (
         <div>
             <h3>사용자 목록</h3>
             <ul>
-                <li><Link to="/profiles/velopert">velopert</Link></li>
-                <li><Link to="/profiles/homer">homer</Link></li>
+                <li><NavLink to="/profiles/velopert" activeStyle={{background: 'black', color:'white'}} activeClassName="active" isActive={(match, location) => {return true;}}>velopert</NavLink></li>
+                <li><NavLink to="/profiles/homer" activeStyle={{background: 'black', color:'white'}}>homer</NavLink></li>
             </ul>
 
             <Route path="/profiles" exact render={() => <div>사용자를 선택해주세요.</div>} />
             <Route path="/profiles/:username" component={Profile} />
+            <WithRouterSample />
         </div>
     );
 }
